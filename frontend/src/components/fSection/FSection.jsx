9b1a9fbb19108e588b1e0085c07119c646844d2b
Youcef Bour <Youcefbour7@gmail.com>
2024-12-14 15:29:06 +0100

    image

---
+++ frontend/src/components/fSection/FSection.jsx
@@ -0,0 +1,18 @@
+function FSection(){
+    return(
+        <div className="text-center mt-5">
+                <h4>Manage your customers using AI </h4>
+                <p className="text-black-50"> Powerful, flexible and data-driven. Polio makes it easy to build the exact CRM your business
+                needs</p>
+                <form className="form-inline my-4 my-lg-4 ">
+          <button className="btn btn btn-outline-dark my-2 my-sm-0 mx-sm-1" type="submit">
+            Demo
+          </button>
+          <button className="btn btn-dark my-2 my-sm-0" type="submit">
+            Start for free
+          </button>
+        </form>
+        </div>
+    )
+}
+export default FSection  ; 
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import StartForFree from '../../components/StartForFree'
+import FSection from '../../components/fSection/FSection'
+
 
 export default function Home() {
   return (
     <div>
-      
+      <FSection />
+      <div className='container p-5'>
+      <img src={`${process.env.REACT_APP_PUBLIC_FOLDER}DashboardD.png`} className=" img-fluid" alt="Responsive image" />
+      </div>
 
       <StartForFree bg={true}/>
     </div>
