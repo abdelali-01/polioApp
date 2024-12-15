d233da826379fa55d7c63e2cf6b32945e9ffde85
abdelali <aliaribi47@gmail.com>
2024-12-15 17:42:10 +0100

    login

--- frontend/src/pages/Sign/SignIn.jsx
+++ frontend/src/pages/Sign/SignIn.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import "./sign.css"
 
 export default function SignIn() {
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
   return (
-    <div>
-      
+    <div className="loginPage text-center">
+      <div className="formCard">
+        <img src={PF + "Logo.svg"} alt="" width={"100"}  className="mb-4"/>
+
+        <h5 className="fw-bold">Sign In </h5>
+        <div className="btnCard mt-3">
+          <span>
+            <img src={PF + "googleIcon.png"} alt="" />
+          </span>
+          <span>Sign in with Google</span>
+        </div>
+        <div className="btnCard mt-3">
+          <span>
+            <img src={PF + "appleIcon.png"} alt="" />
+          </span>
+          <span>Sign in with Apple</span>
+        </div>
+        <br />
+        <p>OR</p>
+        <form action="">
+            <div className="input">
+                <label htmlFor="email">Email</label>
+                <input type="email" placeholder="Enter your email"/>
+            </div>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
